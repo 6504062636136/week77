d5d026f6c81ee9c2bd92af48cc3eafecf5c40d6f
student <s6504062636136@email.kmutnb.ac.th>
2025-01-29 10:48:44 +0700

    a

---
+++ android/app/src/main/kotlin/com/example/week77/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week77
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:week77/f.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyFutureBuilderPage(),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final List<String> products = [
+    'Water Filter RO-01',
+    'Brown Long Sleeve Jacket T01',
+    'Filter 3 Steps',
+    'Smart Robot Car',
+    'Remote Controller'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.separated(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.shopping_cart),
+            title: Text(
+              products[index],
+              style: const TextStyle(fontSize: 20),
+            ),
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Tapped on ${products[index]}')));
+            },
+          );
+        },
+        separatorBuilder: (context, int index) {
+          return const Divider();
+        },
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
